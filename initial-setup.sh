72716b70b71ac8039cc2a5c22eb3e005c68c884f
Ismail Bouajaja <ismail.bouajaja@gmail.com>
2025-09-29 04:46:00 +0200

    add watchtower

--- initial-setup.sh
+++ initial-setup.sh
@@ -1,12 +1,12 @@
 touch .env
 echo "TS_AUTHKEY=CHANGE_THIS_TAILNET_OAUTH_KEY" >> .env
 echo "SEARXNG_SECRET=CHANGE_THIS_SECRET" >> .env
-echo "SEARXNG_HOSTNAME=searxng" >> .env
-echo "TAILNET_DOMAIN=CHANGE_THIS_TAILNET_DOMAIN.ts.net" >> .env
+echo "HOSTNAME=searxng" >> .env
+echo "DOMAIN=CHANGE_THIS_TAILNET_DOMAIN.ts.net" >> .env
 
 sed -i "s|CHANGE_THIS_SECRET|$(openssl rand -hex 32)|g" ./.env
 
-# mkdir data
+mkdir data
 
 echo "Setup complete. You can now run searxng with the command:"
 echo "docker compose up -d"
