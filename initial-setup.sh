38b7a8bc1bb7e886f35104a8589646546e5362c7
Ismail Bouajaja <ismail.bouajaja@gmail.com>
2025-10-11 14:10:20 +0200

    keep data folder tracked by git.

--- initial-setup.sh
+++ initial-setup.sh
@@ -1,8 +1,10 @@
-touch .env
-echo "TS_AUTHKEY=CHANGE_THIS_TAILNET_OAUTH_KEY" >> .env
-echo "SEARXNG_SECRET=CHANGE_THIS_SECRET" >> .env
-echo "HOSTNAME=searxng" >> .env
-echo "DOMAIN=CHANGE_THIS_TAILNET_DOMAIN.ts.net" >> .env
+if [ ! -f .env ]; then
+    touch .env
+    echo "TS_AUTHKEY=CHANGE_THIS_TAILNET_OAUTH_KEY" >> .env
+    echo "SEARXNG_SECRET=CHANGE_THIS_SECRET" >> .env
+    echo "HOSTNAME=searxng" >> .env
+    echo "DOMAIN=CHANGE_THIS_TAILNET_DOMAIN.ts.net" >> .env
+fi
 
 sed -i "s|CHANGE_THIS_SECRET|$(openssl rand -hex 32)|g" ./.env
 
